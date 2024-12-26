f2f8288db53f3d1ee2a9952e9a1541608c38be3b
SArcD <141703135+SArcD@users.noreply.github.com>
2024-12-25 18:04:56 -0600

    Update main.py

--- main.py
+++ main.py
@@ -723,6 +723,9 @@ for i in range(num_cstars):
 #    arc_turbulence, arc_blur, arc_elliptical
 #)
 
+image_size=(image_width, image_height)
+
+
 # Generate filament layers
 filaments_image = Image.new("RGBA", image_size, (0, 0, 0, 0))
 for config in filament_configs:
@@ -764,7 +767,6 @@ for config in bubble_configs:
 
 
 # Generate layers
-image_size=(image_width, image_height)
 #filaments_image = generate_filaments(image_size, centers, num_filaments, filament_radius, filament_length, hex_to_rgb(filament_start_color), hex_to_rgb(filament_end_color), filament_blur, filament_elliptical)
 #diffuse_gas_image = generate_diffuse_gas(image_size, centers, gas_inner_radius, gas_outer_radius, hex_to_rgb(gas_start_color), hex_to_rgb(gas_end_color), gas_blur, gas_elliptical)
 #bubble_image = generate_bubble(image_size, centers, bubble_inner_radius, bubble_outer_radius, hex_to_rgb(bubble_start_color), hex_to_rgb(bubble_end_color), bubble_turbulence, bubble_blur, bubble_elliptical)
